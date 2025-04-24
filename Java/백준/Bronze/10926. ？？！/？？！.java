3daf635fef67c826548c6049e7a87f28cbd114d5
boooobea <100406371+boooobea@users.noreply.github.com>
2025-04-24 15:26:56 +0900

    [Bronze V] Title: ??!, Time: 180 ms, Memory: 17764 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+ //첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str + "??!");
+
+    }
+}
