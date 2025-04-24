d399bcff2da944c61c887e7a49f6226ca26648ae
boooobea <100406371+boooobea@users.noreply.github.com>
2025-04-24 14:09:51 +0900

    [Bronze V] Title: 사칙연산, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+ //첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+
+    }
+}
